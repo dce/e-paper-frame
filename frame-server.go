a4cbbacf0075b70836627f9ebf30c0799291c811
David Eisinger <david.eisinger@gmail.com>
2021-01-08 23:34:51 -0500

    Dithering

--- frame-server.go
+++ frame-server.go
@@ -3,10 +3,14 @@ package main
 import (
     "fmt"
     "html/template"
+    "image"
     "image/jpeg"
     "io/ioutil"
     "github.com/nfnt/resize"
     "github.com/dce/rpi/epd7in5"
+    "github.com/disintegration/gift"
+    "github.com/lestrrat-go/dither"
+    "log"
     "net/http"
     "os"
 )
@@ -19,6 +23,7 @@ var homepage = `<!DOCTYPE html>
         <a href="/photos/{{.Name}}">
           <img src="/thumb?p={{.Name}}">
         </a><br>
+        <a href="/thumb2?p={{.Name}}">Thumb</a>
         <a href="/display?p={{.Name}}">Display</a>
       </p>
     {{end}}
@@ -32,6 +37,7 @@ func main() {
         http.StripPrefix("/thumbs/", http.FileServer(http.Dir("./thumbs"))))
 
     http.HandleFunc("/thumb", Thumbnail)
+    http.HandleFunc("/thumb2", Thumbnail2)
     http.HandleFunc("/display", Display)
     http.HandleFunc("/", ListPhotos)
 
@@ -60,6 +66,41 @@ func Thumbnail(w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, fmt.Sprintf("/thumbs/%s", photo), 301)
 }
 
+func Thumbnail2(w http.ResponseWriter, r *http.Request) {
+    photos, ok := r.URL.Query()["p"]
+    if !ok {
+        fmt.Fprintf(w, "Error: required parameter ('p') not supplied")
+        return
+    }
+
+    photo := photos[0]
+
+    file, err := os.Open(fmt.Sprintf("photos/%s", photo))
+    if err != nil {
+        fmt.Fprintf(w, "Error: %s", err)
+        return
+    }
+    defer file.Close()
+
+    img, err := jpeg.Decode(file)
+    if err != nil {
+        fmt.Fprintf(w, "Error: %s", err)
+        return
+    }
+
+    g := gift.New(
+      gift.ResizeToFill(880, 852, gift.LanczosResampling, gift.CenterAnchor),
+    )
+
+    thumb := image.NewRGBA(g.Bounds(img.Bounds()))
+
+    g.Draw(thumb, img)
+
+    dithered := dither.Monochrome(dither.Burkes, thumb, 1.18)
+
+    jpeg.Encode(w, dithered, nil)
+}
+
 func Display(w http.ResponseWriter, r *http.Request) {
     photos, ok := r.URL.Query()["p"]
     if !ok {
@@ -69,6 +110,7 @@ func Display(w http.ResponseWriter, r *http.Request) {
 
     photo := photos[0]
 
+    log.Println("Starting...")
     epd, _ := epd7in5.New("P1_22", "P1_24", "P1_11", "P1_18")
 
     file, err := os.Open(fmt.Sprintf("photos/%s", photo))
@@ -84,9 +126,15 @@ func Display(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    log.Println("Initializing the display...")
     epd.Init()
+
+    log.Println("Clearing...")
     epd.Clear()
+
+    log.Println("Displaying image...")
     epd.Display(epd.Convert(img))
+
     fmt.Fprintf(w, "Displaying %s", photo)
 }
 
